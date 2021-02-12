81708301d674e5b84abe3cc6a0537feca134a921
kyledobash <kyledobash@yahoo.com>
2021-02-12 10:03:46 -0700

    created function skeleton for put in app.js, added custom css to index.html

--- Client/app.js
+++ Client/app.js
@@ -26,26 +26,51 @@
     $('#my-form').submit( processForm );
 })(jQuery);
 
-$(document).ready(function(){
-   
+
+(function($){
+    function processForm( e ){
+        var dict = {
+        	Title : this["title"].value,
+        	Director: this["director"].value,
+            Genre: this["genre"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'put',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( data );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
+
+    $('#edit').submit( processForm );
+})(jQuery);
+
+
+
+
+
+
+$(document).ready(function(){  
     $.get("https://localhost:44325/api/movie", function(data){        
 
         for(let i = 0; i < data.length; i++){
             $("#movies").append(`<tr>
-            <td>Title: ${JSON.stringify(data[i].title)}</td>
-            <td>Director: ${JSON.stringify(data[i].director)}</td>
-            <td>Genre: ${JSON.stringify(data[i].genre)}</td>
+            <td>${JSON.stringify(data[i].title)}</td>
+            <td>${JSON.stringify(data[i].director)}</td>
+            <td>${JSON.stringify(data[i].genre)}</td>
+            <td><button type="submit" id="edit">Edit</button></td>
             </tr>`);
         }
         
-        // data.map(function(el){
-        //     $("#movies").append(`<div>
-        //     <div>Title: ${JSON.stringify(el.title)}</div>
-        //     <div>Director: ${JSON.stringify(el.director)}</div>
-        //     <div>Genre: ${JSON.stringify(el.genre)}</div>
-        //     </div><br>`);
-        // })
-        //$("#movie1").css("color", "red")
-    // console.log(data);
     })
 })
